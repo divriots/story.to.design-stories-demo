de74b31821c9d4209fb4e0a2513ecb434c89f957
Mikhail Bashkirov <bashmish@gmail.com>
2022-11-16 17:42:44 +0100

    feat(icon): add Icon component

---
+++ icon/stories/icon.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryFn, StoryObj } from '@storybook/react';
+import * as React from 'react';
+import type { IconProps } from '../src/icon';
+import { Icon, iconNames } from '../src/icon';
+
+const meta: Meta = {
+  title: 'Icons/Icon',
+  component: Icon,
+};
+
+export default meta;
+
+export const Playground: StoryObj<IconProps> = {
+  argTypes: {
+    icon: { type: { name: 'string', required: true }, control: 'select', options: iconNames },
+  },
+  args: {
+    icon: iconNames[0],
+  },
+};
+
+export const All: StoryFn<IconProps> = () => (
+  <>
+    {iconNames.map((name, index) => {
+      return <Icon icon={name} key={index} />;
+    })}
+  </>
+);
