280d01dfb9b0b76e337aaf91acef3d87228e0026
Mikhail Bashkirov <bashmish@gmail.com>
2022-11-17 16:19:14 +0100

    fix: subcomponents detection

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -6,10 +6,12 @@ export const decorators = [(renderStory: Function) => <ThemeWrapper>{renderStory
 
 function ThemeWrapper(props: { children: React.ReactNode }) {
   return (
-    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+    <div id="root">
       {/* temporary workaround until S2D supports SB7 default root `#storybook-root` */}
       {/* TODO: remove when `#storybook-root` is supported */}
-      <div id="root">{props.children}</div>
-    </MantineProvider>
+      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+        {props.children}
+      </MantineProvider>
+    </div>
   );
 }
