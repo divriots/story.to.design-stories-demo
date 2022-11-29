986f1ec7e868cced0bc636a850f8900c9fa059a1
Mikhail Bashkirov <bashmish@gmail.com>
2022-11-29 14:47:26 +0000

    fix: remove SB7 workaround

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -6,12 +6,8 @@ export const decorators = [(renderStory: Function) => <ThemeWrapper>{renderStory
 
 function ThemeWrapper(props: { children: React.ReactNode }) {
   return (
-    <div id="root">
-      {/* temporary workaround until S2D supports SB7 default root `#storybook-root` */}
-      {/* TODO: remove when `#storybook-root` is supported */}
-      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-        {props.children}
-      </MantineProvider>
-    </div>
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      {props.children}
+    </MantineProvider>
   );
 }
